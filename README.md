586a518941c1f8a08a7d8e43c5033d0c8563433a
Justin Grewal <jgrew@users.noreply.github.com>
2023-02-09 21:23:10 -0800

    remove redundant config init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# discharge-notification
+# irwm-hub-table
+
+Reusable ESRI table widget with a download button. Multiple tables can be configured by specifying a url parameter.
--- src/views/Layout.view.svelte
+++ src/views/Layout.view.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import "@esri/calcite-components/dist/components/calcite-shell";
   import { Table } from "$components/index";
   import { useAppStore } from "$store/index";
 
-  const { layoutStore, configStore } = useAppStore();
+  const { layoutStore } = useAppStore();
 
-  onMount(()=>{
-    configStore.actions.init();
-  })
 </script>
 
 <calcite-shell content-behind>
